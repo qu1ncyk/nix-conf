fce9be6130ffb6a07928621fb93e52c468147fe2
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2024-03-12 15:03:36 +0100

    Moved the workspace logic to ``sway/boot.sh`

--- config/sway/boot.sh
+++ config/sway/boot.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+swaymsg workspace 10
+
 safeeyes &
 wl-gammarelay-rs &
 ~/.config/sway/blue-filter &
 syncthing --no-browser &
 nm-applet --no-agent &
 keepassxc &
-bash
+
+sleep 1
+swaymsg workspace 1
