e293967b11b06a6b5abf2351c8dc3749b11df894
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2024-12-16 10:03:05 +0100

    Added more Nix snippets

--- user/lazy-too/lua/snippets/nix.lua
+++ user/lazy-too/lua/snippets/nix.lua
@@ -72,7 +72,7 @@ return {
           shellHook = "<>";
         }
       ]],
-      { i(1, "pkgs.hello"), i(2, "hello") }
+      { i(1, "hello"), i(2, "hello") }
     )
   ),
   s(
@@ -83,7 +83,7 @@ return {
           targetPkgs = ps: (with ps; [<>]);
         }).env
       ]],
-      { i(1, "pkgs.hello") }
+      { i(1, "hello") }
     )
   ),
   s(
@@ -114,16 +114,6 @@ return {
       { i(1, "name"), i(2), i(3, "false"), i(4, "/share/file") }
     )
   ),
-  unpack(vim.tbl_map(function(v)
-    return s(
-      "write" .. v,
-      fmta([[
-        pkgs.write]] .. v .. [[ "<>" ''
-          <>
-        ''
-      ]], { i(1, "name"), i(2) })
-    )
-  end, { "Text", "TextDir", "Script", "ScriptBin", "ShellScript", "ShellScriptBin" })),
   s(
     "symlinkJoin",
     fmta(
@@ -137,6 +127,103 @@ return {
       { i(1), i(2), i(3) }
     )
   ),
+  s(
+    "mkDerivation",
+    fmta(
+      [[
+        stdenv.mkDerivation {
+          name = "<>";
+          src = <>;
+
+          nativeBuildInputs = [<>];
+          buildInputs = [<>];
+
+          dontBuild = <>;
+          installPhase = ''<>'';
+        }
+      ]],
+      { i(1, "package-1.0"), i(2, "./."), i(3, "makeWrapper"), i(4, "ncurses"), i(5, "true"), i(6) }
+    )
+  ),
+  s(
+    "pmkDerivation",
+    fmta(
+      [[
+        stdenv.mkDerivation rec {
+          pname = "<>";
+          version = "<>";
+          src = <>;
+
+          nativeBuildInputs = [<>];
+          buildInputs = [<>];
+
+          dontBuild = <>;
+          installPhase = ''<>'';
+        }
+      ]],
+      { i(1, "package"), i(2, "1.0"), i(3, "./."), i(4, "makeWrapper"), i(5, "ncurses"), i(6, "true"), i(7) }
+    )
+  ),
+  s(
+    "makeWrapper",
+    fmta(
+      [[
+        makeWrapper <> $out/bin/<> \
+          --add-flags "<>"
+      ]],
+      { i(1), i(2, "program"), i(3) }
+    )
+  ),
+  s(
+    "substitute",
+    fmta(
+      [[
+        substitute <> <> \
+          --replace <> <> \
+          --subst-var <>
+      ]],
+      { i(1, "src.txt"), i(2, "dest.txt"), i(3, "pattern"), i(4, "replacement"), i(5, "env-var") }
+    )
+  ),
+  s(
+    "substituteInPlace",
+    fmta(
+      [[
+        substituteInPlace <> \
+          --replace <> <> \
+          --subst-var <>
+      ]],
+      { i(1, "file.txt"), i(2, "pattern"), i(3, "replacement"), i(4, "env-var") }
+    )
+  ),
+  s(
+    "substituteAll",
+    fmta(
+      [[
+        substituteAll <> <>
+      ]],
+      { i(1, "src.txt"), i(2, "dest.txt") }
+    )
+  ),
+  s(
+    "substituteAllInPlace",
+    fmta(
+      [[
+        substituteAllInPlace <>
+      ]],
+      { i(1, "file.txt") }
+    )
+  ),
   s("if", fmta("if <> then <> else <>", { i(1, "cond"), i(2, "if-true"), i(3, "if-false") })),
   s("let", fmta("let <> = <>; in <>", { i(1, "var"), i(2, "val"), i(3) })),
+  unpack(vim.tbl_map(function(v)
+    return s(
+      "write" .. v,
+      fmta([[
+        pkgs.write]] .. v .. [[ "<>" ''
+          <>
+        ''
+      ]], { i(1, "name"), i(2) })
+    )
+  end, { "Text", "TextDir", "Script", "ScriptBin", "ShellScript", "ShellScriptBin" })),
 }
