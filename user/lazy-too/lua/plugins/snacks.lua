7316fd864fd97b5134debda7159f9843c8bebb5a
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2025-10-07 13:34:05 +0200

    Made `snacks.image` only appear in Markdown files

--- user/lazy-too/lua/plugins/snacks.lua
+++ user/lazy-too/lua/plugins/snacks.lua
@@ -13,7 +13,18 @@ return {
         conceal = function()
           return false
         end,
+        enabled = false,
       },
     },
   },
+  config = function(plugin, opts)
+    local snacks = require("snacks")
+    snacks.setup(opts)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function(args)
+        snacks.image.doc.attach(args.buf)
+      end,
+    })
+  end,
 }
