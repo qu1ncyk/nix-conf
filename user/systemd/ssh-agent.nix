0ad665ca54fa452508cd8b74b51cb1c0e7a1ec17
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2026-05-05 18:25:54 +0200

    Added a program for SSH askpass

--- user/systemd/ssh-agent.nix
+++ user/systemd/ssh-agent.nix
@@ -4,6 +4,7 @@
     Install.WantedBy = ["default.target"];
     Service.ExecStart = pkgs.writeShellScript "ssh-agent" ''
       rm -f /tmp/ssh-agent.socket
+      export SSH_ASKPASS=${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass
       ${pkgs.openssh}/bin/ssh-agent -D -a /tmp/ssh-agent.socket
     '';
   };
