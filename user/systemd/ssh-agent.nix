f499e8b28a075594dd169c88479138271eb22790
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2026-05-10 18:37:19 +0200

    Restart ssh agent on error

--- user/systemd/ssh-agent.nix
+++ user/systemd/ssh-agent.nix
@@ -2,10 +2,14 @@
   systemd.user.services.ssh-agent = {
     Unit.Description = "ssh-agent";
     Install.WantedBy = ["default.target"];
-    Service.ExecStart = pkgs.writeShellScript "ssh-agent" ''
-      rm -f /tmp/ssh-agent.socket
-      export SSH_ASKPASS=${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass
-      ${pkgs.openssh}/bin/ssh-agent -D -a /tmp/ssh-agent.socket
-    '';
+    Service = {
+      Restart = "on-failure";
+      RestartSec = "1s";
+      ExecStart = pkgs.writeShellScript "ssh-agent" ''
+        rm -f /tmp/ssh-agent.socket
+        export SSH_ASKPASS=${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass
+        ${pkgs.openssh}/bin/ssh-agent -D -a /tmp/ssh-agent.socket
+      '';
+    };
   };
 }
